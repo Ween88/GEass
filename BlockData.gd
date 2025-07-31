891fe0bfdb779bc0a77e81145ee6224a4ec36199
Ween88 <alweensiow0828@1utar.my>
2025-08-01 01:35:20 +0800

    Allow Mining but only 1 type of block

---
+++ BlockData.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name BlockData
+
+@export var tile_name: String = "default_block"
+@export var health: int = 2
+@export var atlas_coords: Array [Vector2i] = []
+@export var source1: int = 4
---
+++ terra_spark.gd
@@ -0,0 +1,73 @@
+extends Node2D
+
+@export var block :  Dictionary[String, BlockData]
+@onready var ground = $Ground
+@export var player : CharacterBody2D
+
+var broken_tiles_health : Dictionary
+var current_block = "soil"
+var distance : float = INF
+
+var inventory = {
+	"soil" : 0
+}
+
+func _physics_process(_delta):
+	if player:
+		distance = (get_global_mouse_position() - player.position).length()
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		var tile_pos = get_snapped_position(get_global_mouse_position())
+		
+		if event.button_index == MOUSE_BUTTON_LEFT and distance < 100:
+			print(ground.get_cell_atlas_coords(tile_pos))
+			var data = ground.get_cell_tile_data(tile_pos)
+			var tile_name
+			if data:
+				tile_name = data.get_custom_data("tile_name")
+				print(tile_name)
+				print(block[tile_name].health)
+				take_damage(tile_name, tile_pos)
+		
+		if is_placeable(event):
+			ground.set_cell(tile_pos, block[current_block].source1, block[current_block].atlas_coords[0])
+			inventory[current_block] -= 1
+	if event is InputEventKey:
+		switch_block(event)
+	
+func get_snapped_position(global_pos: Vector2) -> Vector2i:
+	var local_pos = ground.to_local(global_pos)
+	var tile_pos = ground.local_to_map(local_pos)
+	return tile_pos
+
+func take_damage (tile_name : StringName, tile_pos : Vector2i, amount : float = 1):
+	if tile_pos not in broken_tiles_health:
+		broken_tiles_health[tile_pos] = block[tile_name].health - amount
+	else:
+		broken_tiles_health[tile_pos] -= amount
+		
+	print(broken_tiles_health[tile_pos])
+	
+	var difference = block[tile_name].health - broken_tiles_health[tile_pos]
+	var next_tile : Vector2i
+	
+	if difference >= block[tile_name].health:
+		ground.erase_cell(tile_pos)
+		broken_tiles_health.erase(tile_pos)
+		if tile_name in inventory:
+			inventory[tile_name] += 1
+		else:
+			inventory[tile_name] = 1
+	elif difference< block[tile_name].atlas_coords.size():
+		next_tile = block[tile_name].atlas_coords[difference]
+		ground.set_cell(tile_pos, block[tile_name].source1, next_tile)
+		
+	print(broken_tiles_health)
+	
+func switch_block(event):
+	if event.keycode == KEY_1 and event.pressed:
+		current_block = "soil"
+
+func is_placeable(event) -> bool:
+	return event.button_index == MOUSE_BUTTON_RIGHT and distance < 100 and distance > 50 and inventory[current_block] > 0
