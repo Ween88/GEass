f17a77e4f3c386d152f20a76112146e35ed3f99e
Fergus <hadizz0607@gmail.com>
2025-08-25 20:15:31 +0800

    Fixed script

--- HealthBarLogic.gd
+++ HealthBarLogic.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var heal_button = $CanvasLayer/TabBarBackground/heal
 @onready var hurt_button = $CanvasLayer/TabBarBackground2/hurt
 
+var game_over_scene = "res://game_over.tscn"
 var hp = 10
 
 func _ready():
@@ -28,3 +29,4 @@ func hurt_pressed():
 	print("Hurt! HP:", hp)
 	if hp == 0:
 		print("You are defeated!")
+		get_tree().change_scene_to_file(game_over_scene)
